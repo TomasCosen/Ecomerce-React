2877e62ddda6921e33a9f8f81e727271e92a56c8
Tomas Cosentino <tomascosentino123@gmail.com>
2023-10-12 01:45:18 -0300

    intento de BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import "./App.css";
 import NavBar from "./components/NavBar.jsx";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   const greeting = "Bienvenido a Dietética Silvia";
   return (
     <>
-      <NavBar />
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<ItemListContainer />} />
+          <Route path="/category/:id" element={<ItemListContainer />} />
+          <Route path="/item/:id" element={<ItemDetailContainer />} />
+        </Routes>
+      </BrowserRouter>
 
       <ItemListContainer greetings={greeting} />
     </>
